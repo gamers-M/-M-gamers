3c79d587b1574597736ed74b24cda60343d82a23
hanzo6ix <125126141+hanzo6ix@users.noreply.github.com>
2023-04-17 10:46:15 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2023_04_17_084354) do
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.string "postcode", null: false
-    t.string "address", null: false
+    t.string "addresses", null: false
     t.string "name", null: false
   end
 
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 2023_04_17_084354) do
   create_table "cart_items", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "customer_id"
-    t.integer "item_id"
-    t.integer "quantity"
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
+    t.integer "quantity", null: false
   end
 
   create_table "customers", force: :cascade do |t|
@@ -97,17 +97,4 @@ ActiveRecord::Schema.define(version: 2023_04_17_084354) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
 end
