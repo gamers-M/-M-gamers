426f07f097c6415486e6c254a872bb0be6492f7b
EC2 Default User <ec2-user@ip-172-31-23-76.ap-northeast-1.compute.internal>
2023-04-17 06:36:44 +0000

    注文詳細

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_17_054833) do
+ActiveRecord::Schema.define(version: 2023_04_17_062932) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 2023_04_17_054833) do
   create_table "cart_items", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "customer_id"
-    t.integer "item_id"
-    t.integer "quantity"
+    t.integer "customer_id", null: false
+    t.integer "item_id", null: false
+    t.integer "quantity", null: false
   end
 
   create_table "customers", force: :cascade do |t|
@@ -60,6 +60,16 @@ ActiveRecord::Schema.define(version: 2023_04_17_054833) do
     t.text "explanation", null: false
     t.integer "price", null: false
     t.boolean "is_active", default: false, null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "quantity", null: false
+    t.integer "price", null: false
+    t.integer "making_status", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
