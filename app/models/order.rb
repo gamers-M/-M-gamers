da82bf2e03de19f03fad3769983f6f86126cd0c7
hanzo6ix <125126141+hanzo6ix@users.noreply.github.com>
2023-04-18 06:27:25 +0000

    aso

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
   has_many :order_details
+  belongs_to :customer
   enum payment_method: { credit_card: 0, transfer: 1 }
-  
+
   enum status: { waiting_for_production: 0, payment_confirmation: 1, in_production: 2, shipping_preparation: 3, sent: 4 }
-  
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,7 @@
 class OrderDetail < ApplicationRecord
+  belongs_to :item
+  belongs_to :order
+
   enum making_status: { cannot_start: 0, waiting_for_production: 1,
   in_production: 2, completion_of_production: 3 }
 end
