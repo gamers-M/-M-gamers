1e655da9b742f851c9bf7e21e31dee5c6c5770d3
EC2 Default User <ec2-user@ip-172-31-23-76.ap-northeast-1.compute.internal>
2023-04-20 08:34:15 +0000

    ページ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,31 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
+    @customer = Custermer.find(parmas[:id])
   end
-
+    
   def edit
+    @customer = Custermer.find(parmas[:id])
   end
 
   def update
+    @customer = Custermer.find(params[:id])
+    if @customer.update(custermer_params)
+      redirect_to admin_customer_path(@custermer.id)
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def custermer_params
+    params.require(:custermer).permit(:email, :last_name, :first_name,
+    :last_name_kana, :first_name_kana, :postcode, :address,
+    :phone_number, :is_deleted
+    )
   end
 end
