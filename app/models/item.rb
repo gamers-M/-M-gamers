b83ca6090594906248ba1b1cde13c6e3f799bbc1
hanzo6ix <125126141+hanzo6ix@users.noreply.github.com>
2023-04-22 07:11:42 +0000

    admin_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,11 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_details
 
-
   has_one_attached :image
+
+  def add_tax_price
+    (self.price * 1.10).round
+  end
 
   def get_image(width,height)
     unless image.attached?
